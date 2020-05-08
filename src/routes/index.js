260c979c9052808500540964a7e5a652fc6e7286
Anthony Kiplimo <anthony.kiplimo@africastalking.com>
2020-05-08 09:40:37 +0300

    feat: Error handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
